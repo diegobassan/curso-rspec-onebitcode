3f14242c50a6f4eca87c728d70aea0b6677fe90b
Diego Bassan <bsn.diego@gmail.com>
2019-04-06 19:07:44 -0300

    add gem Factory Bot in Tests

---
+++ spec/factories/user.rb
@@ -0,0 +1,9 @@
+require 'faker'
+
+FactoryBot.define do
+    factory :user do
+        nickname { Faker::Lorem.word }
+        level { rand(1..99) }
+        kind { %i[knight wizard].sample }
+    end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,17 +3,11 @@ require 'faker'
 
 RSpec.describe User, type: :model do
   it "is invalid if the level is not between 1 and 99" do
-    nickname = Faker::Name.name
-    kind = %i[knight wizard].sample
-    level = rand(100...999)
-    user = User.new(nickname: nickname, kind: kind, level: level)
+    user = build(:user,level: rand(100..999))
     expect(user).to_not be_valid
   end
   it "is valid hero" do
-    nickname = Faker::Name.name
-    kind = %i[knight wizard].sample
-    level = rand(1...99)
-    user = User.new(nickname: nickname, kind: kind, level: level)
+    user = build(:user,level: rand(1..99))
     expect(user).to be_valid
   end
   it "returns the correct hero title" do
