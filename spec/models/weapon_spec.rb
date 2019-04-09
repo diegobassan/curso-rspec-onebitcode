b9fa782c80bcc4c0ef927d402027c11652496034
Diego Bassan <bsn.diego@gmail.com>
2019-04-08 23:16:59 -0300

    Add Test Current Power

--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -1,12 +1,20 @@
-require 'rails_helper'
-require 'faker'
+require "rails_helper"
+require "faker"
 
 RSpec.describe Weapon, type: :model do
   it "returns the correct weapon title" do
     name = Faker::Name.name
     level = rand(1...99)
-    weapon = Weapon.create(name: name,level: level)
+    weapon = Weapon.create(name: name, level: level)
     expect(weapon.title).to eq("#{name}##{level}")
   end
 
+  it "returns the correct weapon current power" do
+    power_base = 2
+    level = 3
+    power_step = 3
+    weapon = build(:weapon, level: level, power_base: power_base,
+                            power_step: power_step)
+    expect(weapon.current_power).to eq(8)
+  end
 end
